e3426b6e1c74a1a27fee4cf661c14fa6328ec92c
Mohamed Keita <mohkeita13@gmail.com>
2020-09-28 21:22:37 +0200

    init and configure cloud config

--- order-service/src/main/java/io/mohkeita/orderservice/service/OrderService.java
+++ order-service/src/main/java/io/mohkeita/orderservice/service/OrderService.java
@@ -6,17 +6,25 @@ import io.mohkeita.orderservice.common.TransactionResponse;
 import io.mohkeita.orderservice.entity.Order;
 import io.mohkeita.orderservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository repository;
 
     @Autowired
+    @Lazy
     private RestTemplate template;
+
+    @Value("{microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
         String response = "";
@@ -25,7 +33,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         // rest call
-        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
 
         response = paymentResponse.getPaymentStatus().equals("success")?"payment processing successful and order placed":"there is a failure in payment api, order added to cart";
         repository.save(order);
